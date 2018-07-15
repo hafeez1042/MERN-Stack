d219ac551b23d27ca08738949244560324649c4e
Hafeez <hafeez@ceribs.com>
2018-07-15 18:03:59 +0530

    removed config from server and added config.example

--- config.example.js
+++ config.example.js
@@ -1,4 +1,4 @@
 module.exports = {
   JWT_SECRET: 'GjsH&GD2AKC4jhCHydHC2jkdiS3pMcdfgkGs6',
-  mongoDBURL: 'mongodb://root:root@ds237120.mlab.com:37120/merntest',
+  mongoDBURL: 'mongodb://localhost:37120/mernApp',
 };
